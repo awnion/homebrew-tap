2e4162aa8f13be1c22befe6ce6a1d783883abc28
Sergei Blinov <blinovsv@gmail.com>
2026-03-22 23:47:57 +0100

    afio 0.0.17

---
+++ Formula/font-afio.rb
@@ -0,0 +1,29 @@
+class FontAfio < Formula
+  desc "Custom Iosevka Nerd Font with hand-picked glyph variants"
+  homepage "https://github.com/awnion/custom-iosevka-nerd-font"
+  version "0.0.17"
+  url "https://github.com/awnion/custom-iosevka-nerd-font/releases/download/v#{version}/afio-#{version}.zip"
+  sha256 "849f903fd1e3efce22644587043588d3142b6002ca2b580281ac3eb628bad746"
+  license any_of: ["MIT", "Apache-2.0"]
+
+  def install
+    (share/"fonts/afio").install Dir["*.ttf"]
+  end
+
+  def post_install
+    if OS.mac?
+      user_fonts = Pathname.new("#{ENV["HOME"]}/Library/Fonts")
+      user_fonts.mkpath
+      (share/"fonts/afio").each_child do |font|
+        target = user_fonts/font.basename
+        target.unlink if target.exist?
+        FileUtils.cp(font, target)
+      end
+    end
+  end
+
+  test do
+    assert_predicate share/"fonts/afio", :directory?
+    assert_match(/\.ttf$/, Dir[share/"fonts/afio/*"].first)
+  end
+end
